81976490b9f7d984f382a459b97188ea1ec52166
Sangin <sangin.kang@navercorp.com>
2019-05-30 07:26:30 +0900

    navigation

---
+++ navigation/lib/converter_route.dart
@@ -0,0 +1,48 @@
+// Copyright 2018 The Chromium Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+import 'package:task_04_navigation/unit.dart';
+
+class ConverterRoute extends StatelessWidget {
+  final String name;
+  final Color color;
+  final List<Unit> units;
+
+  const ConverterRoute({
+    @required this.name,
+    @required this.color,
+    @required this.units,
+  })  : assert(name != null),
+        assert(color != null),
+        assert(units != null);
+
+  @override
+  Widget build(BuildContext context) {
+    final unitWidgets = units.map((Unit unit) {
+      return Container(
+        color: color,
+        margin: EdgeInsets.all(8.0),
+        padding: EdgeInsets.all(16.0),
+        child: Column(
+          children: <Widget>[
+            Text(
+              unit.name,
+              style: Theme.of(context).textTheme.headline,
+            ),
+            Text(
+              'Conversion: ${unit.conversion}',
+              style: Theme.of(context).textTheme.subhead,
+            ),
+          ],
+        ),
+      );
+    }).toList();
+
+    return ListView(
+      children: unitWidgets,
+    );
+  }
+}
